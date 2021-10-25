7f27ce093a5436dfc0ed73a44195527dc122afd8
Benjamin Gafford <bgafford@bucatini.isri.cmu.edu>
2021-10-25 14:36:31 -0400

    fixed build cmd

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 git submodule update --init
 
-docker build -t scenario-runner
+docker build . -t scenario-runner
 
 # To run (with display)
 # docker run --rm -it --net=host -e DISPLAY=$DISPLAY scenic-runner <cmd>
